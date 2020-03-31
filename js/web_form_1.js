384aa0697ea66f39d6c8384f0b3b5fab49595997
Kaleab <kaleabTadewos@users.noreply.github.com>
2020-03-31 17:32:39 -0500

    add validators and fix some of the issues

--- js/web_form_1.js
+++ js/web_form_1.js
@@ -3,9 +3,5 @@ document.getElementById("btn-submit").addEventListener("click", function() {
     let password = document.getElementById("pword").value;
     let customerWebsite = document.getElementById("customerWebsite").value;
 
-    console.log("Email of user is : " + email);
-    console.log("Password of user is : " + password);
-    console.log("Website of user is : " + customerWebsite);
-
     alert("Email of user is : " + email + "\n Password of user is : " + password + "\n" + "Website of user is : " + customerWebsite);
 });
